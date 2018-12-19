79a1ea59b6c356413f1bd8c4bc3e3b912c387c01
Milena Kari <milenakari@yahoo.com>
2018-12-19 23:36:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song 
+  @@all = []
+  def initialize(name, artist,genre)
+    @name = name
+    @artist = artist
+    @genre=genre
+    @@all << self
+  end
+  def self.all 
+    @@all
+  end
+end
