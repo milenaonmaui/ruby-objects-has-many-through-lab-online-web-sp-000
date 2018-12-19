da99dd30eddbf744fefe547dc60c5327f40799ca
Milena Kari <milenakari@yahoo.com>
2018-12-19 23:44:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,12 @@
-class Patient 
-  @@all = [] 
-  attr_reader :name 
+class Patient
+  @@all = []
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @@all << self
   end
-  def self.all 
+  def self.all
     @@all
   end
 end
