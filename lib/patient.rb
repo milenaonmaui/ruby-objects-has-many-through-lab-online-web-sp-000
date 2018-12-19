fb153bbe345e13209e3a26085bbe5cfc821113a3
Milena Kari <milenakari@yahoo.com>
2018-12-19 23:57:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,4 +13,11 @@ class Patient
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
+
+  def appointments 
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end 
+  end 
+    
 end
